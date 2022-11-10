b9f8887d7d7acf19e0d39f1521e33363b5a5f92c
Christian J. Atencio Peña <christianatencio@upeu.edu.pe>
2022-11-09 19:22:18 -0500

    christian

---
+++ src/components/ModalAlumno.vue
@@ -0,0 +1,117 @@
+<template>
+    <TransitionRoot appear :show="show" as="template">
+      <Dialog as="div" @close="closeModal" class="relative z-10">
+        <TransitionChild
+          as="template"
+          enter="duration-300 ease-out"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="duration-200 ease-in"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div class="fixed inset-0 bg-black bg-opacity-25" />
+        </TransitionChild>
+  
+        <div class="fixed inset-0 overflow-y-auto">
+          <div
+            class="flex min-h-full items-center justify-center p-4 text-center"
+          >
+            <TransitionChild
+              as="template"
+              enter="duration-300 ease-out"
+              enter-from="opacity-0 scale-95"
+              enter-to="opacity-100 scale-100"
+              leave="duration-200 ease-in"
+              leave-from="opacity-100 scale-100"
+              leave-to="opacity-0 scale-95"
+            >
+              <DialogPanel
+                class="w-full max-w-lg transform overflow-hidden text-left align-middle shadow-xl transition-all"
+              >
+                <div
+                  class="bg-primary text-white font-medium px-4 py-2 rounded-t-lg"
+                >
+                  <DialogTitle as="h3" class="text-lg">
+                    REGISTRAR ESTUDIANTE
+                  </DialogTitle>
+                </div>
+                <div class="bg-white rounded-b-lg">
+                  <div class="grid gap-x-8 gap-y-4 grid-cols-2 px-4">
+                    <div>
+                      <label for="" class="form-label">CÓDIGO: </label>
+                      <input
+                        type="text"
+                        placeholder="Ingrese Código Estudiante"
+                        class="bg-gray-100 rounded-lg focus:outline-none px-4 py-2 w-full"
+                      />
+                    </div>
+                    <div >
+                      <label for="" class="form-label">NOMBRE:</label>
+                      <input
+                        type="text"
+                        placeholder="Ingrese Nombre"
+                        class="bg-gray-100 rounded-lg focus:outline-none px-4 py-2 w-full"
+                      />
+                    </div>
+                  </div>
+  
+                  <div class="flex justify-end gap-2 p-4">
+                    <button
+                      @click="closeModal"
+                      class="text-gray-600 text-sm shadow px-6 py-2 rounded-lg font-medium"
+                    >
+                      Cancelar
+                    </button>
+                    <button
+                      class="bg-primary text-white text-sm shadow px-6 py-2 rounded-lg font-medium"
+                    >
+                      Guardar
+                    </button>
+                  </div>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+  </template>
+  
+  <script>
+  import {
+    Dialog,
+    DialogBackdrop,
+    DialogTitle,
+    DialogDescription,
+    DialogOverlay,
+    DialogPanel,
+    TransitionChild,
+    TransitionRoot,
+  } from "@headlessui/vue";
+  export default {
+    components: {
+      Dialog,
+      DialogBackdrop,
+      DialogTitle,
+      DialogDescription,
+      DialogOverlay,
+      DialogPanel,
+      TransitionChild,
+      TransitionRoot,
+    },
+    data: () => ({}),
+    props: {
+      show: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    methods: {
+      closeModal() {
+        this.$emit("closed");
+      },
+    },
+  };
+  </script>
+  
--- src/views/Dashboard/Asistencias.vue
+++ src/views/Dashboard/Asistencias.vue
@@ -1,11 +1,14 @@
 <template>
+  <alumno-modal @closed="closeModal" :show="modal"></alumno-modal>
   <div class="grid gap-4">
-    <div
-      class="rounded-lg bg-gray-100 shadow py-4 text-2xl font-bold text-secondary text-center"
-    >
+    <div class="rounded-lg bg-gray-100 shadow py-4 text-2xl font-bold text-secondary text-center">
       Activate Sistemas
     </div>
-    <div class="flex justify-between items-center w-full">
+
+    <button class="bg-secondary text-white rounded-lg px-4 py-2 shadow whitespace-nowrap" @click="aperturar">
+          APERTURAR ASISTENCIA
+        </button>
+    <!-- <div class="flex justify-between items-center w-full">
       <div class="flex gap-4">
         <select class="form-control w-36" v-model="form.semestre">
           <option value="">Semestre</option>
@@ -25,31 +28,124 @@
       >
         Nuevo Registro
       </button>
+    </div> -->
+
+    <div class="w-full overflow-x-auto" v-if="this.activate">
+      <div class="flex gap-4">
+        <select class="form-control w-36" v-model="form.semestres2" @change="onChange($event)">
+          <option value="">Semestre</option>
+          <option v-for="d in semestres2" :value="d.id">
+            {{ d.semestre }}
+          </option>
+        </select>
+
+        <select class="form-control w-36" v-model="form.ciclo">
+          <option value="" hidden>Ciclo</option>
+          <option v-for="c in ciclos2" :value="c.id">
+            {{ c.name }}
+          </option>
+        </select>
+
+        <button class="bg-secondary text-white rounded-lg px-4 py-2 shadow whitespace-nowrap" @click="openPage">
+          FILTRAR
+        </button>
+      </div>
+
+      <br>
+
+
+
+      <div class="flex gap-4" v-if="this.regEst">
+
+        <div>
+          <label for="" class="form-label">Nombres: </label>
+          <input type="text" v-model="form.name" class="bg-gray-100 rounded-lg focus:outline-none px-4 py-2 w-full"
+            @keyup="keyUp($event)" list="students"/>
+          <datalist id="students" style="width: 50%">
+            <option v-for="c in alumnos">{{ c.codigo }} - {{ c.nombre }} {{ c.apellido }}</option>
+          </datalist>
+        </div>
+
+        <!-- <select class="form-control w-96" v-model="form.codigo" @change="addRegister($event)">
+          <option value="">Estudiantes</option>
+          <option v-for="a in alumnos" :value="a.id">
+            {{ a.codigo }} - {{ a.nombre }} {{ a.apellido }}
+          </option>
+        </select> -->
+        <div>
+          <button class="bg-secondary text-white rounded-lg px-4 py-2 shadow whitespace-nowrap" @click="openModal">
+            Registrar Alumnos
+          </button>
+        </div>
+      </div>
+
+      <br>
+
+      <div class="flex gap-4" v-if="this.alumno_asistencia">
+        <div>
+          <label for="" class="form-label">Nombres: </label>
+          <input type="text" v-model="studentForm.name"
+            class="bg-gray-100 rounded-lg focus:outline-none px-4 py-2 w-full" disabled />
+        </div>
+
+        <div>
+          <label for="" class="form-label">Apellidos: </label>
+          <input type="text" v-model="studentForm.lastame"
+            class="bg-gray-100 rounded-lg focus:outline-none px-4 py-2 w-full" disabled />
+        </div>
+
+        <div>
+          <label for="" class="form-label">Fecha: </label>
+          <input type="date" v-model="studentForm.fecha"
+            class="bg-gray-100 rounded-lg focus:outline-none px-4 py-2 w-full" />
+        </div>
+
+        <div>
+          <!-- <label><input type="checkbox" v-model="studentForm.asistencia" @change="aer($event)"> ASISTIÓ </label> -->
+          <label class="switch">
+            <input type="checkbox" v-model="studentForm.asistencia" @change="aer($event)">
+            <span class="slider round"></span>
+          </label>
+          <!-- <input type="text" v-model="studentForm.fecha"
+            class="bg-gray-100 rounded-lg focus:outline-none px-4 py-2 w-full" /> -->
+
+        </div>
+
+        <div>
+          <button class="bg-secondary text-white rounded-lg px-4 py-2 shadow whitespace-nowrap" @click="asistencia">
+            Registrar Asistencia
+          </button>
+        </div>
+      </div>
     </div>
+
     <div class="w-full overflow-x-auto">
-        <table>
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Apellido</th>
-                    <th>Fecha Del Viernes</th>
-                    <th>Correo</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Jezer Gabriel</td>
-                    <td>Rázuri Pichén</td>
-                    <td>Martes</td>
-                    <td>jezerrazuri@upeu.edu.oe</td>
-                    <td>
-                       Eliminar
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Fecha Del Viernes</th>
+            <th>Código</th>
+            <th>¿Asistió?</th>
+          </tr>
+        </thead>
+        <br>
+        <tbody>
+          <tr v-for="a in table">
+            <td>{{ a.id }}</td>
+            <td>{{ a.nombre }}</td>
+            <td>{{ a.apellido }}</td>
+            <td>{{ a.fecha }}</td>
+            <td>{{ a.codigo == true ? 'SI' : 'NO' }}</td>
+            <td>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+
   </div>
 </template>
 
@@ -59,6 +155,15 @@ export default {
     form: {
       semestre: "",
       ciclo: "",
+      semestres2: '',
+      alumno: '',
+      codigo: ''
+    },
+    studentForm: {
+      name: '',
+      lastame: '',
+      asistencia: '',
+      fecha: '',
     },
     semestres: [
       { id: 1, name: "Semestre 1" },
@@ -76,12 +181,224 @@ export default {
       {
         id: 1,
         name: "Ciclo 1",
+        semestre: '1'
       },
       {
         id: 2,
         name: "Ciclo 2",
+        semestre: '2'
+      },
+      {
+        id: 3,
+        name: "Ciclo 3",
+        semestre: '1'
+      },
+      {
+        id: 4,
+        name: "Ciclo 4",
+        semestre: '2'
+      },
+      {
+        id: 5,
+        name: "Ciclo 5",
+        semestre: '1'
+      },
+      {
+        id: 6,
+        name: "Ciclo 6",
+        semestre: '2'
+      },
+      {
+        id: 7,
+        name: "Ciclo 7",
+        semestre: '1'
+      },
+      {
+        id: 8,
+        name: "Ciclo 8",
+        semestre: '2'
+      },
+      {
+        id: 9,
+        name: "Ciclo 9",
+        semestre: '1'
+      },
+      {
+        id: 10,
+        name: "Ciclo 10",
+        semestre: '2'
       },
     ],
+
+    semestres2: [
+      { id: 1, semestre: '2022 - 1' },
+      { id: 2, semestre: '2022 - 2' },
+      { id: 3, semestre: '2023 - 1' },
+      { id: 4, semestre: '2023 - 2' },
+      { id: 5, semestre: '2024 - 1' },
+      { id: 6, semestre: '2024 - 2' },
+      { id: 7, semestre: '2025 - 1' },
+      { id: 8, semestre: '2025 - 2' }
+    ],
+    alumnos: [
+      { id: 1, nombre: 'Christian Josue', apellido: 'Atencio Peña', codigo: '201810191'  , ciclo : 1},
+      { id: 2, nombre: 'Carlos', apellido: 'Maldonado Curo', codigo: '201810192'  , ciclo : 10},
+      { id: 3, nombre: 'Sefora Abigail', apellido: 'Alarcón Llori', codigo: '201810193'  , ciclo : 1},
+      { id: 4, nombre: 'Magdiel Manuela', apellido: 'Carranza Gomez', codigo: '2014785'  , ciclo : 5},
+      { id: 5, nombre: 'Sulema Diana', apellido: 'Añamuro Lopez', codigo: '2021141232'  , ciclo : 7},
+      { id: 6, nombre: 'Kevin Manuel', apellido: 'Palacín Irrivarren', codigo: '2017859674'  , ciclo : 2},
+      { id: 7, nombre: 'Salomon Hugo', apellido: 'Pietro Maximoff', codigo: '2016852145' , ciclo : 7 },
+      { id: 8, nombre: 'Josue Andre', apellido: 'Molina Gutierrez', codigo: '2016928364'  , ciclo : 2},
+    ],
+    table: [
+    ],
+    modal: false,
+    ciclos2: [],
+    regEst: false,
+    alumno_asistencia: false,
+    cont: 0,
+    al_ciclo : [],
+    activate : false
   }),
+  // mounted() {
+  //   this.openPage()
+  // },
+  methods: {
+    aperturar(){
+      this.activate = true
+    },
+    onChange(event) {
+      let semestre = JSON.parse(JSON.stringify(this.semestres2.find(e => e.id == event.target.value))).semestre
+      this.ciclos2 = this.ciclos
+      let sm = semestre.split('-')[1].trim()
+      this.ciclos2 = JSON.parse(JSON.stringify(this.ciclos2.filter(e => e.semestre == sm)))
+    },
+    openModal() {
+      this.modal = true;
+    },
+    closeModal() {
+      this.modal = false;
+    },
+    openPage() {
+      if (this.alumnos.length === 0) {
+        alert('NO SE ENCUENTRAN REGISTRADOS ALUMNOS EN EL CICLO, PROCEDA A AGREGAR ALUMNOS')
+        this.regEst = true
+        this.openModal()
+      } else {
+        alert('HAY ALUMNOS REGISTRADOS')
+        this.al_ciclo = this.alumnos.filter(e => e.ciclo == this.form.ciclo)
+        this.regEst = true
+
+
+      }
+    },
+    addRegister(event) {
+      let student = JSON.parse(JSON.stringify(this.alumnos.find(e => e.id == event.target.value)))
+      console.log(student);
+
+      this.alumno_asistencia = true
+
+      this.studentForm.name = student.nombre
+      this.studentForm.lastame = student.apellido
+      // this.studentForm.name=student.nombre
+
+    },
+    aer(event) {
+      console.log(event.target.checked);
+      console.log(this.studentForm.asistencia);
+    },
+    keyUp(event) {
+      let code = event.target.value.split('-')[0].trim()
+      console.log(code);
+
+      let encontrado = JSON.parse(JSON.stringify(this.alumnos.find(e => e.codigo == code)))
+
+      this.alumno_asistencia = true
+      if (encontrado != undefined) {
+        //   this.studentForm.name = "REGISTRO NO ENCONTRADO"
+        this.studentForm.name = encontrado.nombre
+        this.studentForm.lastame = encontrado.apellido
+        // this.studentForm.lastame = "REGISTRO NO ENCONTRADO"
+
+
+      }
+      // console.log(encontrado);
+
+
+
+    },
+    asistencia() {
+      this.cont++
+      this.table.push({
+        id: this.cont,
+        nombre: this.studentForm.name,
+        apellido: this.studentForm.lastame,
+        fecha: this.studentForm.fecha,
+        codigo: this.studentForm.asistencia
+      })
+    }
+  }
 };
 </script>
+
+<style>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked+.slider {
+  background-color: #2196F3;
+}
+
+input:focus+.slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked+.slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
